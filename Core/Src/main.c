afec8e859b2f37c84a61c302f7b028c2106ee394
Tom Chassing <tomchassing19@gmail.com>
2026-05-19 18:41:03 +0200

    Correction + Test de la µSD

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,6 +24,8 @@
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h> //for va_list var arg functions
+
 #include "ssd1306.h"
 #include "ssd1306_fonts.h"
 #include "BME280_STM32.h"
@@ -52,6 +54,8 @@ SPI_HandleTypeDef hspi3;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
+#define SPI_CS_GPIO_Port GPIOB
+#define SPI_CS_Pin GPIO_PIN_0
 
 /* USER CODE END PV */
 
@@ -62,12 +66,25 @@ static void MX_USART2_UART_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_SPI3_Init(void);
 /* USER CODE BEGIN PFP */
-
+void myprintf(const char *fmt, ...);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+FATFS FatFs;
+FIL fil;
 
+void myprintf(const char *fmt, ...) {
+  static char buffer[256];
+  va_list args;
+  va_start(args, fmt);
+  vsnprintf(buffer, sizeof(buffer), fmt, args);
+  va_end(args);
+
+  int len = strlen(buffer);
+  HAL_UART_Transmit(&huart2, (uint8_t*)buffer, len, -1);
+
+}
 /* USER CODE END 0 */
 
 /**
@@ -100,11 +117,15 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
-  MX_I2C1_Init();
+ // MX_I2C1_Init();
   MX_SPI3_Init();
   MX_FATFS_Init();
   /* USER CODE BEGIN 2 */
-  int status = BME280_Config (OSRS_2, OSRS_16, MODE_NORMAL, T_SB_0p5, IIR_16);
+  
+  /*------------------------------------------
+  Capteur de temperature et de pression BMP280
+  --------------------------------------------*/
+ /* int status = BME280_Config (OSRS_2, OSRS_16, MODE_NORMAL, T_SB_0p5, IIR_16);
   if (status!= 0)
     {
   	  Error_Handler();
@@ -113,7 +134,154 @@ int main(void)
   BME280_WakeUP();
   float temp=0;
   float press=0;
+ */
+  /*------------------------------------------
+  Carte SD + FatFS demo
+  --------------------------------------------*/
+  myprintf("TEST SPI EN COURS ...\r\n");
 
+
+
+
+
+
+
+
+
+  //Force CS high
+
+  HAL_GPIO_WritePin(SPI_CS_GPIO_Port, SPI_CS_Pin, GPIO_PIN_SET);
+
+  HAL_Delay(10);
+
+
+
+  // Envoie 10 octets 0xFF et affiche ce qu'on reçoit
+
+  uint8_t tx = 0xFF, rx = 0x00;
+
+  myprintf("Dummy bytes received: ");
+
+  for(int i = 0; i < 10; i++) {
+
+	  HAL_SPI_TransmitReceive(&hspi3, &tx, &rx, 1, 100);
+
+	  myprintf("%02X ", rx);
+
+  }
+
+  myprintf("\r\n");
+
+
+
+  // Force CS low et envoie CMD0
+
+  HAL_GPIO_WritePin(SPI_CS_GPIO_Port, SPI_CS_Pin, GPIO_PIN_RESET);
+
+  HAL_Delay(1);
+
+  uint8_t cmd0[] = {0x40, 0x00, 0x00, 0x00, 0x00, 0x95};
+
+  uint8_t resp[7] = {0};
+
+  HAL_SPI_Transmit(&hspi3, cmd0, 6, 100);
+
+  // Lire 7 octets de réponse
+
+  for(int i = 0; i < 7; i++) {
+
+	  HAL_SPI_TransmitReceive(&hspi3, &tx, &resp[i], 1, 100);
+
+	  myprintf("resp[%d] = %02X\r\n", i, resp[i]);
+
+  }
+
+  HAL_GPIO_WritePin(SPI_CS_GPIO_Port, SPI_CS_Pin, GPIO_PIN_SET);
+
+
+  myprintf("\r\n~ SD card demo by kiwih ~\r\n\r\n");
+
+  HAL_Delay(1000); //a short delay is important to let the SD card settle
+
+  //some variables for FatFs
+  FATFS FatFs; 	//Fatfs handle
+  FIL fil; 		//File handle
+  FRESULT fres; //Result after operations
+
+  //Open the file system
+  fres = f_mount(&FatFs, "", 1); //1=mount now
+  if (fres != FR_OK) {
+  myprintf("f_mount error (%i)\r\n", fres);
+  //while(1);
+  }
+
+  
+  //Let's get some statistics from the SD card
+  DWORD free_clusters, free_sectors, total_sectors;
+
+  FATFS* getFreeFs;
+
+
+  fres = f_getfree("", &free_clusters, &getFreeFs);
+  if (fres != FR_OK) {
+  myprintf("f_getfree error (%i)\r\n", fres);
+  //while(1);
+  }
+
+  //Formula comes from ChaN's documentation
+  total_sectors = (getFreeFs->n_fatent - 2) * getFreeFs->csize;
+  free_sectors = free_clusters * getFreeFs->csize;
+
+  myprintf("SD card stats:\r\n%10lu KiB total drive space.\r\n%10lu KiB available.\r\n", total_sectors / 2, free_sectors / 2);
+
+  //Now let's try to open file "test.txt"
+  fres = f_open(&fil, "TEST.TXT", FA_READ);
+  if (fres != FR_OK) {
+  myprintf("f_open error (%i)\r\n", fres);
+  //while(1);
+  }
+  else {
+  myprintf("I was able to open 'test.txt' for reading!\r\n");
+  }
+  //Read 30 bytes from "test.txt" on the SD card
+  BYTE readBuf[30];
+
+  //We can either use f_read OR f_gets to get data out of files
+  //f_gets is a wrapper on f_read that does some string formatting for us
+  TCHAR* rres = f_gets((TCHAR*)readBuf, 30, &fil);
+  if(rres != 0) {
+  myprintf("Read string from 'test.txt' contents: %s\r\n", readBuf);
+  } else {
+  myprintf("f_gets error (%i)\r\n", fres);
+  }
+
+  //Be a tidy kiwi - don't forget to close your file!
+  f_close(&fil);
+
+  //Now let's try and write a file "write.txt"
+  fres = f_open(&fil, "write.txt", FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
+  if(fres == FR_OK) {
+  myprintf("I was able to open 'write.txt' for writing\r\n");
+  } else {
+  myprintf("f_open error (%i)\r\n", fres);
+  }
+
+  //Copy in a string
+  strncpy((char*)readBuf, "a new file is made!", 20);
+  UINT bytesWrote;
+  fres = f_write(&fil, readBuf, 19, &bytesWrote);
+  if(fres == FR_OK) {
+  myprintf("Wrote %i bytes to 'write.txt'!\r\n", bytesWrote);
+  } else {
+  myprintf("f_write error (%i)\r\n", fres);
+  }
+
+  //Be a tidy kiwi - don't forget to close your file!
+  f_close(&fil);
+
+  //We're done, so de-mount the drive
+  f_mount(NULL, "", 0);
+  
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -123,7 +291,11 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  BME280_Measure(&temp,&press);
+
+  /*------------------------------------------
+  Partie capteur de temperature et de pression BMP280
+  --------------------------------------------*/
+  /*BME280_Measure(&temp,&press);
 
   char mess[100];
   sprintf(mess, "T: %.2f C, P: %.2f hPa \r\n", temp, press);
@@ -143,7 +315,9 @@ int main(void)
   ssd1306_WriteString(strPress, Font_7x10, White);
 
   ssd1306_UpdateScreen();
+
   HAL_Delay(2000);
+  */
   }
   /* USER CODE END 3 */
 }
@@ -344,7 +518,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = SPI3_CS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(SPI3_CS_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
